de2fc2abec8801a85ea267c5da57c674a96cb464
sashasemeniuk <sasha.sos.06032009@gmail.com>
2026-05-14 22:42:35 +0300

    rm frontend_copy

--- frontend copy/lib/index.ts
+++
@@ -1,3 +0,0 @@
-// React Book Reader - A beautiful, mobile-first book reading experience
-export { BookReader } from './BookReader';
-export type { BookReaderProps, ContentRegion } from './BookReader';
